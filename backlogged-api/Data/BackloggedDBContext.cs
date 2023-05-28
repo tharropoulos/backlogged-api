8301b59c120e01c57241171c1af257b805de8978
Fanis Tharropoulos <ftharropoulos@gmail.com>
2023-05-29 00:11:15 +0300

    feat: ✨ add db context

---
+++ backlogged-api/Data/BackloggedDBContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using backlogged_api.Models;
+using Microsoft.Extensions.Configuration;
+using Npgsql;
+using Microsoft.Extensions.Options;
+using System.Reflection.Metadata;
+
+namespace backlogged_api.Data
+{
+    public class BackloggedDBContext : DbContext
+    {
+        protected readonly IConfiguration Configuration;
+        public BackloggedDBContext(DbContextOptions<BackloggedDBContext> options, IConfiguration configuration) : base(options)
+        {
+            Configuration = configuration;
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseNpgsql(Configuration.GetConnectionString("DefaultConnection"));
+    }
+}
--- backlogged-api/Program.cs
+++ backlogged-api/Program.cs
@@ -9,7 +9,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-
+builder.Services.AddDbContext<BackloggedDBContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
