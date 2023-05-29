cedc1f9dd370d9d5e1776969acb5f0b7a83afcd6
Fanis Tharropoulos <ftharropoulos@gmail.com>
2023-05-29 16:55:25 +0300

    fix(models): 🐛 make user object required for Backlog

--- backlogged-api/Models/Backlog.cs
+++ backlogged-api/Models/Backlog.cs
@@ -4,8 +4,8 @@
     {
         public Guid id { get; set; }
         public required Guid userId { get; set; }
-        public User user { get; set; }
-        public ICollection<Game>?  games { get; set; }
+        public required User user { get; set; }
+        public ICollection<Game>? games { get; set; }
         public Boolean isVisible { get; set; } = false;
 
     }
