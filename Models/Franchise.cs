722e5737d2acef5a8c3a7e7aa9f0399bc9001488
Fanis Tharropoulos <tharrokav00@gmail.com>
2023-05-31 16:58:32 +0300

    feat(helpers): ✨ add helpers for pagination

--- Models/Franchise.cs
+++ Models/Franchise.cs
@@ -2,7 +2,6 @@
 {
     public record Franchise : BaseEntity
     {
-        public Guid id { get; set; }
         public required string name { get; set; }
         public ICollection<Game>? games { get; set; }
     }
