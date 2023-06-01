25825d0a5feda23aebab24e6ae2bee75c8b64a27
Fanis Tharropoulos <tharrokav00@gmail.com>
2023-06-01 19:47:20 +0300

    feat(dto): ✨ add main Backlog DTO

---
+++ DTO/Backlog/BacklogDto.cs
@@ -0,0 +1,14 @@
+using backlogged_api.DTO.Game;
+using backlogged_api.Helpers;
+
+namespace backlogged_api.DTO.Backlog
+{
+    public record BacklogDto
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public Boolean IsVisible { get; set; } = false;
+        public PagedList<GameDto>? Games { get; set; }
+
+    }
+}
