faf017b0e6cbbe5c196cc4907b5f1b6db79f1cf3
Fanis Tharropoulos <tharrokav00@gmail.com>
2023-06-06 16:32:18 +0300

    feat(controller): ✨ add controller wide authorization for Backlogs

--- Controllers/BacklogsController.cs
+++ Controllers/BacklogsController.cs
@@ -12,12 +12,14 @@ using backlogged_api.Helpers;
 using Newtonsoft.Json;
 using System.Linq.Expressions;
 using backlogged_api.DTO.Game;
+using Microsoft.AspNetCore.Authorization;
 
 namespace backlogged_api.Controllers
 {
     [Route("api/v1/[controller]")]
     [ApiController]
     [ApiVersion("1.0")]
+    [Authorize(AuthenticationSchemes = "Bearer")]
     public class BacklogsController : ControllerBase
     {
         private readonly BackloggedDBContext _context;
