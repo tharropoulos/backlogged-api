a6cba01ba0b50406c27a81b76ec346b6388b9d1f
Fanis Tharropoulos <tharrokav00@gmail.com>
2023-06-09 20:57:34 +0300

    refactor(controller): ♻️  change Genre to use singular form for Games endpoints

--- Controllers/GenresController.cs
+++ Controllers/GenresController.cs
@@ -203,7 +203,7 @@ namespace backlogged_api.Controllers
         /// <response code="200">Games</response>
         /// <response code="404">Backlog not found</response>
         // Get: api/Backlogs/uuid/Games
-        [HttpGet("{id}/Games")]
+        [HttpGet("{id}/Game")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<ActionResult<IEnumerable<GameDto>>> GetGames(Guid id, [FromQuery] PagingParams pagingParams)
